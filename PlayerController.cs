1658eac597abba5ff40afd93acfc84917e5338cd
lillybo <lillian.bojkov@gmail.com>
2023-08-01 14:54:16 +0300

    GDB-55 adding player controller

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,21 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    float playerYPos;
-
+    // Start is called before the first frame update
     void Start()
     {
-        playerYPos = transform.position.y;
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            playerYPos = -playerYPos;
-
-            transform.position = new Vector3(transform.position.x, playerYPos, transform.position.z);
-        }
+        
     }
 }

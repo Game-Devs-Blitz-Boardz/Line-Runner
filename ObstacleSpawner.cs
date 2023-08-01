967cd7035facc164e8cddef0cb3f354d624c954b
lillybo <lillian.bojkov@gmail.com>
2023-08-01 17:26:30 +0300

    GDB-55 spawning obstacles at bottom

--- ObstacleSpawner.cs
+++ ObstacleSpawner.cs
@@ -22,8 +22,24 @@ public class ObstacleSpawner : MonoBehaviour
 
     void Spawn() {
         int spawnEl = Random.Range(0, obstacles.Length);
+        int spawnSide = Random.Range(0, 2);
 
-        Instantiate(obstacles[spawnEl], spawnPos, transform.rotation);
+        spawnPos = transform.position;
+
+        if (spawnSide < 1) {
+            Instantiate(obstacles[spawnEl], spawnPos, transform.rotation);
+        } else {
+            // spawnPos = new Vector3(transform.position.x, -transform.position.y, 180);
+            spawnPos.y = -transform.position.y;
+            int xAdd = spawnEl;
+            spawnPos.x += xAdd;
+
+            GameObject obs = Instantiate(obstacles[spawnEl], spawnPos, transform.rotation);
+            obs.transform.eulerAngles = new Vector3(0, 0, 180);
+        }
+
+
+        
     }
 
     IEnumerator SpawnObstacles() {

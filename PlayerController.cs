c3bd56abe83d5354283b2063dbb87dcb3f71c000
lillybo <lillian.bojkov@gmail.com>
2023-08-01 14:51:53 +0300

    GDB-55 <message>

---
+++ PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float playerYPos;
+
+    void Start()
+    {
+        playerYPos = transform.position.y;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            playerYPos = -playerYPos;
+
+            transform.position = new Vector3(transform.position.x, playerYPos, transform.position.z);
+        }
+    }
+}

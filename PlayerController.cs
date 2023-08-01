b63fd1596932def883150ed0083c928696793241
lillybo <lillian.bojkov@gmail.com>
2023-08-01 14:58:23 +0300

    GDB-55 adding content to player controller script

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    float playerYPos;
+
     void Start()
     {
-        
+        playerYPos = transform.position.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) {
+            playerYPos = -playerYPos;
+
+            transform.position = new Vector3(transform.position.x, playerYPos, transform.position.x);
+        }
     }
 }
